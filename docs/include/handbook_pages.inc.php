fa5d4243f077e0662713d15b2870a71193790604
Greg Gay <info@atutor.ca>
2010-06-18 12:39:18 +0000

    update admin handbook pages

--- docs/include/handbook_pages.inc.php
+++ docs/include/handbook_pages.inc.php
@@ -11,23 +11,37 @@
 /************************************************************************/
 
 $handbook_pages = array(
+		'home/index.php',
                'register.php',
                'login.php',
                'password_reminder.php',
+               'profile/index.php' =>   array(
+                                        'profile/change_password.php',
+                                        'profile/change_email.php'
+                                        ),
                'user/index.php' =>      array(
                                         'user/user_create_edit.php',
                                         'user/user_password.php',
                                         'user/user_group.php',
                                         'user/user_group_create_edit.php'   
                                         ),
+		'home/create_course.php' => array('home/course/course_property.php'),
+		'tests/index.php' => array('tests/create_test.php',
+					   'tests/edit_test.php',
+					   'tests/questions.php',
+					   'tests/question_db.php',
+					   'tests/question_cats.php'),
+
+		'home/editor/edit_content.php' => array('home/editor/edit_content_folder.php',
+					  'home/editor/arrange_content.php',
+					  'home/editor/import_export_content.php',
+					  'home/editor/delete_content.php',
+					  'file_manager/index.php'),
                'language/index.php' =>  array(
                                         'language/language_add_edit.php'
                                         ),
                'translation/index.php' => array(),
-               'profile/index.php' =>   array(
-                                        'profile/change_password.php',
-                                        'profile/change_email.php'
-                                        ),
+
                'updater/index.php' => array('updater/patch_create.php')
 );
 
