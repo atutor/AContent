fea8653e5a09036f1fe0b61791db7f5677665ce1
Cindy Li <cli@ocad.ca>
2010-06-28 16:56:57 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=4375

--- docs/include/sidemenus/my_courses.inc.php
+++ docs/include/sidemenus/my_courses.inc.php
@@ -17,7 +17,7 @@ require_once(TR_INCLUDE_PATH.'classes/DAO/UserCoursesDAO.class.php');
 // can only be used by login user
 if (!isset($_SESSION['user_id']) || $_SESSION['user_id'] == 0) return;
 
-global $savant;
+global $savant, $_course_id;
 
 $userCoursesDAO = new UserCoursesDAO();
 $output = '';
@@ -34,7 +34,8 @@ if (!is_array($my_courses)) {
 	$num_of_courses = count($my_courses);
 
     $output .= '<ol class="remove-margin-left">'."\n";
-	foreach ($my_courses as $row) {
+	
+    foreach ($my_courses as $row) {
 		// only display the first 200 character of course description
 
 		if ($row['role'] == TR_USERROLE_AUTHOR) {
@@ -42,7 +43,7 @@ if (!is_array($my_courses)) {
 		} else {
 			$output .= ' <li class="theirs" title="'. _AT('others_course').': '. $row['title'].'">'."\n";
 		}
-		$output .= '    <a href="'. TR_BASE_HREF.'home/course/index.php?_course_id='. $row['course_id'].'">'.$row['title'].'</a>'."\n";
+		$output .= '    <a href="'. TR_BASE_HREF.'home/course/index.php?_course_id='. $row['course_id'].'"'.(($_course_id == $row['course_id']) ? ' class="selected-sidemenu"' : '').'>'.$row['title'].'</a>'."\n";
 		if ($row['role'] == TR_USERROLE_VIEWER) {
 			$output .= '    <a href="'. TR_BASE_HREF.'home/'. $caller_url.'action=remove&cid='. $row['course_id'].'">'."\n";
             $output .= '      <img src="'. TR_BASE_HREF.'themes/'. $_SESSION['prefs']['PREF_THEME'].'/images/delete.gif" alt="'. htmlspecialchars(_AT('remove_from_list')).'" title="'. htmlspecialchars(_AT('remove_from_list')).'" border="0" />'."\n";
@@ -56,11 +57,11 @@ if (!is_array($my_courses)) {
 		//$output .= '    <a href="'. TR_BASE_HREF.'home/ims/ims_export.php?course_id='. $row['course_id'].'">'."\n";
 		//$output .= '      <img src="'. TR_BASE_HREF.'themes/'. $_SESSION['prefs']['PREF_THEME'].'/images/export.png" alt="'. _AT('download_content_package').'" title="'. _AT('download_content_package').'" border="0" />'."\n";
 		//$output .= '    </a>'."\n";
-		if ($row['role'] == TR_USERROLE_AUTHOR) {
+		//if ($row['role'] == TR_USERROLE_AUTHOR) {
 			//$output .= '    <a href="'. TR_BASE_HREF.'home/imscc/ims_export.php?course_id='. $row['course_id'].'">'."\n";
 			//$output .= '      <img src="'. TR_BASE_HREF.'themes/'. $_SESSION['prefs']['PREF_THEME'].'/images/export_cc.png" alt="'. _AT('download_common_cartridge').'" title="'. _AT('download_common_cartridge').'" border="0" />'."\n";
 			//$output .= '    </a>'."\n";
-		}
+		//}
 		$output .= '  </li>'."\n";				
 	} // end of foreach; 
     $output .= '</ol>'."\n";
