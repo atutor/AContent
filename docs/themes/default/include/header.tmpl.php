b065625486ef8279923abd827295cad4e52d4f6c
Greg Gay <info@atutor.ca>
2010-08-08 15:18:30 +0000

    replaced go back icon

--- docs/themes/default/include/header.tmpl.php
+++ docs/themes/default/include/header.tmpl.php
@@ -363,7 +363,7 @@ if (is_array($this->categories)) { // print category dropdown list box?>
     <div id="sub-navigation">
 	  <?php if (isset($this->back_to_page)): ?>
 	    <div id="subnavbacktopage">	  
-	      <a href="<?php echo $this->back_to_page['url']; ?>" id="back-to"><?php echo '<img src="'.TR_BASE_HREF.'images/arrowicon.gif"  alt="'._AT('back_to').':'.$this->back_to_page['title'].'" title="'._AT('back_to').':'.$this->back_to_page['title'].'" style="vertical-align:center;" />'; ?></a> 
+	      <a href="<?php echo $this->back_to_page['url']; ?>" id="back-to"><?php echo '<img src="'.TR_BASE_HREF.'images/arrowicon.png"  alt="'._AT('back_to').':'.$this->back_to_page['title'].'" title="'._AT('back_to').':'.$this->back_to_page['title'].'" style="vertical-align:center;" />'; ?></a> 
 	    </div>
 	  <?php endif; ?>
 	<ul id="subnavlist">
