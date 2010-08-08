82251922d176e4666b7bf56added28c37fc29bee
Greg Gay <info@atutor.ca>
2010-08-08 00:05:54 +0000

    rewrite some documention, still needs work.

--- docs/documentation/web_service_api.php
+++ docs/documentation/web_service_api.php
@@ -18,11 +18,10 @@ include(TR_INCLUDE_PATH.'header.inc.php');
 <div class="output-form" style="line-height:150%">
 
 <h1>AContent Web Service API</h1>
-<p>Interface applications with the AContent through its experimental API. This is version 0.1, dated Jun 2010.</p>
-<p>AContent provides search web service API that allows users to send request to AContent to search through 
-the repository by keywords. AContent returns the search results in REST format. AContent also provides URLs and 
-<a href="<?php echo TR_BASE_HREF;?>documentation/oauth_web_server">OAuth web service API</a> to view, download, import, 
-export courses at AContent. </p>
+<p>Access AContent from remote applications through its web service API. This is version 0.1, dated Jun 2010.</p>
+<p>AContent provides a search API that allows users to send search requests to AContent. AContent returns results in REST format. AContent also has an 
+<a href="<?php echo TR_BASE_HREF;?>documentation/oauth_web_server">OAuth web service API</a> for viewing, downloading, importing and 
+exporting content from remote applications. </p>
 
 <h2 id="TableOfContents">Table of Contents</h2>
 
@@ -47,12 +46,12 @@ export courses at AContent. </p>
 <div id="search">
 <h2 id="request_format">Search request format</h2>
 
-<p>Below is a table of the parameter you can use to send a request to AContent to search through the repository
+<p>Below is a table of the parameters you can use to send a request to AContent to search through the repository
 by keywords. AContent returns the matching results in REST format.</p>
 
 <p>URL:<br />
 <kbd><?php echo TR_BASE_HREF; ?>search.php</kbd> <br />
-(replace with the address of your own server if you want to call a private instance of the AContent)</p>
+(replace with the address of your own server if you want to call a private instance of AContent)</p>
 
 <table class="data" rules="all">
 <tbody><tr>
@@ -62,7 +61,7 @@ by keywords. AContent returns the matching results in REST format.</p>
 <tr>
   <th>id</th>
   <td>The "Web Service ID" generated once successfully registering into AContent. 
-  This ID is a 32 characters long string. It can always be retrieved from user's "Profile" page.</td>
+  This ID is a 32 characters long string. It can always be found on your "Profile" page.</td>
   <td>None, must be given.</td>
 </tr>
 
@@ -153,7 +152,7 @@ by keywords. AContent returns the matching results in REST format.</p>
 
 <div id="urls">
 <h2 id="view">View course URL</h2>
-<kbd><?php echo TR_BASE_HREF;?>home/course/index.php?_course_id=10</kbd><br />
+<kbd><?php echo TR_BFSE_HREF;?>home/course/index.php?_course_id=10</kbd><br />
 <p>_course_id is the "courseID" element returned in the REST search result</p>
 <p>This URL leads to the first content page of the course.</p>
 
@@ -164,23 +163,23 @@ by keywords. AContent returns the matching results in REST format.</p>
 </div>
 
 <h2 id="import">Import common cartridge or content package into AContent</h2>
-<p>A AContent user ID with author privilege is required for this action. If you
-don't have an AContent ID yet, register at <a href="<?php echo TR_BASE_HREF;?>register.php">here</a>.</p> 
+<p>An AContent user ID with author privilege is required for this action. If you
+don't have an AContent ID yet, <a href="<?php echo TR_BASE_HREF;?>register.php">register</a>.</p> 
 
-<p>Once the AContent user ID is ready, the first step is to go through 
+<p>After an AContent user ID has been created, the first step is to go through the
 <a href="<?php echo TR_BASE_HREF;?>documentation/oauth_server_api.php">
-AContent OAuth authentication process</a> to get an OAuth access token. The 
-authentication process prompts user to login with the AContent user ID and an
-access token is returned at the end of the authentication process. This access 
-token must be sent along with the import request for user privilege check. The access
-token can be used repeatedly till its expire threshold is reached. After the
-user is confirmed as an valid author, the common cartridge or content package retrieved
-from the request "url" parameter is imported as a new AContent course. The course
+OAuth authentication process</a> to get an OAuth access token. The 
+authentication process prompts users to login with an AContent user ID, after which an
+access token is returned. This access 
+token must be sent along with the import request as a user privilege check. The access
+token can be used repeatedly until its expiration. After the
+user is confirmed as an valid author, the common cartridge or content package is retrieved
+from the request "url" parameter and imported as a new AContent course. The course
 ID of the newly-imported course is returned at success.</p>
 
 <kbd><?php echo TR_BASE_HREF;?>home/ims/ims_import.php?oauth_token=xxx&url=xxx</kbd><br />
 
-<p>oauth_token is the valid access token returned by AContent OAuth server.<br />
+<p>oauth_token is the valid access token returned by an AContent OAuth server.<br />
 url points to a zip file of a common cartridge or a content package.<br />
 Refer to <a href="<?php echo TR_BASE_HREF;?>documentation/oauth_server_api.php#import">
 Import Common Cartridge or Content Package into AContent via OAuth</a> for detailed 
