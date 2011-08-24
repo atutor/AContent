127737c614c1ab4acfabdb3e2e464f171869022a
Cindy Li <cli@ocad.ca>
2011-08-24 16:37:18 +0000

    http://atutor.ca/atutor/mantis/view.php?id=4829

--- docs/documentation/index.php
+++ docs/documentation/index.php
@@ -41,7 +41,7 @@ if (isset($_GET['p'])) {
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo DEFAULT_LANGUAGE_CODE; ?>" lang="<?php echo DEFAULT_LANGUAGE_CODE; ?>"> 
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title><?php echo htmlspecialchars(_AT('Transformable_handbook'), ENT_QUOTES, 'UTF-8'); ?></title>
 
 <script type="text/javascript">
