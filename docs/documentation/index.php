cb322d68c402f4ddfd1260fe9d9703b2ebbb9d84
Cindy Li <cli@ocad.ca>
2011-08-24 16:14:02 +0000

    http://atutor.ca/atutor/mantis/view.php?id=4829 Wrap up handbook title with UTF-8 version of htmlspecialchars().

--- docs/documentation/index.php
+++ docs/documentation/index.php
@@ -42,7 +42,7 @@ if (isset($_GET['p'])) {
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo DEFAULT_LANGUAGE_CODE; ?>" lang="<?php echo DEFAULT_LANGUAGE_CODE; ?>"> 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-	<title><?php echo _AT('Transformable_handbook'); ?></title>
+	<title><?php echo htmlspecialchars(_AT('Transformable_handbook'), ENT_QUOTES, 'UTF-8'); ?></title>
 
 <script type="text/javascript">
 
