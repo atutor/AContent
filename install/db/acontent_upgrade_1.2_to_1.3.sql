01ea082bc728033540da491e732e925669939624
gregrgay <info@atutor.ca>
2012-12-04 11:53:53 -0500

    added sql updates to accomodate bolgna templating system

--- install/db/acontent_upgrade_1.2_to_1.3.sql
+++ install/db/acontent_upgrade_1.2_to_1.3.sql
@@ -2,3 +2,12 @@
 UPDATE `users` SET `is_author` = '1' WHERE `user_id` = '2;
 // give admin access to tests and file manager
 INSERT INTO `user_group_privilege` (`user_group_id` ,`privilege_id` ,`user_requirement`) VALUES ('1', '8', '0'), ('1', '9', '0');
+
+// Updates for AContent templates
+
+ALTER TABLE `content` ADD `layout` TEXT NOT NULL AFTER `content_type` 
+ALTER TABLE `content` ADD `structure` TEXT NOT NULL AFTER `optional` 
+
+INSERT INTO `config`(`name`, `value`) VALUES ('enable_template_structure','1');
+INSERT INTO `config`(`name`, `value`) VALUES ('enable_template_layout','1');
+INSERT INTO `config`(`name`, `value`) VALUES ('enable_template_page','1');
