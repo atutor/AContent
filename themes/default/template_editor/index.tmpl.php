e704e4aabad34da3b3ddea364d84e2258e6f7e96
atutor <info@atutor.ca>
2014-09-14 11:06:43 -0400

    5528: fixed typos in selected tab check

--- themes/default/template_editor/index.tmpl.php
+++ themes/default/template_editor/index.tmpl.php
@@ -5,9 +5,9 @@
             <?php 
                 if($this->template_type=="layout") echo '<li class="active"><b>'. _AT('layout') . '</b></li>';
                 else echo '<li><a href="'. $_SERVER['PHP_SELF'] . '?tab=layout">'. _AT('layout') . '</a></li>';
-                if($this->template_type=="structure") echo '<li class="active"><b>'. _AT('structures') . '</b></li>';
+                if($this->template_type=="structures") echo '<li class="active"><b>'. _AT('structures') . '</b></li>';
                 else echo '<li><a href="'. $_SERVER['PHP_SELF'] . '?tab=structures">'. _AT('structures') . '</a></li>';
-                if($this->template_type=="page") echo '<li class="active"><b>'. _AT('pages') . '</b></li>';
+                if($this->template_type=="pages") echo '<li class="active"><b>'. _AT('pages') . '</b></li>';
                 else echo '<li><a href="'. $_SERVER['PHP_SELF'] . '?tab=pages">'. _AT('pages') . '</a></li>';
             ?>
         </ul>
