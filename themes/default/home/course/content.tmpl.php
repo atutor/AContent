f6b4551bfc58fbb0566d02ba4a269a447842e148
Greg Gay <gregrgay@gmail.com>
2017-05-28 13:09:15 -0400

    removed extra content div

--- themes/default/home/course/content.tmpl.php
+++ themes/default/home/course/content.tmpl.php
@@ -94,10 +94,8 @@ $result=$dao->execute($sql);
     if($choice_layout!=''){
         ?>
         <div id="content">
-          <!--  <div id="content"> -->
         <link  rel="stylesheet" href="<?php echo TR_BASE_HREF; ?>templates/layouts/<?php echo $choice_layout; ?>/<?php echo $choice_layout; ?>.css"  type="text/css" />
                     <?php echo $this->body; ?>
-           <!-- </div> -->
         </div>
         <?php
     }else{ ?>
