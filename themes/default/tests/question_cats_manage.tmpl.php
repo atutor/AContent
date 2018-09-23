d8007c92097a6c2e0e1fe0352516082e73a72134
Metamorfosec <33624021+metamorfosec@users.noreply.github.com>
2018-09-24 06:42:42 +0700

    Remove csrf_class, add htmlspecialchars...

--- themes/default/tests/question_cats_manage.tmpl.php
+++ themes/default/tests/question_cats_manage.tmpl.php
@@ -9,10 +9,6 @@
 /* modify it under the terms of the GNU General Public License          */
 /* as published by the Free Software Foundation.                        */
 /************************************************************************/
-
-session_start();
-
-require_once(TR_ClassCSRF_PATH.'class_csrf.php');
 ?>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="form">
@@ -26,11 +22,10 @@ if (isset($this->catid)) {
 	<fieldset class="group_form"><legend class="group_form"><?php echo $this->title; ?></legend>
 	<div class="row">
 		<span class="required" title="<?php echo _AT('required_field'); ?>">*</span><label for="cat"><?php echo _AT('title'); ?></label><br />
-		<input type="text" name="title" id="cat" value="<?php if (isset($_POST['title']) AND CSRF_Token::isValid() AND CSRF_Token::isRecent()) echo htmlspecialchars($_POST['title']); else echo htmlspecialchars($this->row['title']); ?>" />
+		<input type="text" name="title" id="cat" value="<?php echo htmlspecialchars(trim(stripslashes(strip_tags($_POST['title'])))); ?>" />
 	</div>
 
 	<div class="row buttons">
-		<?php echo CSRF_Token::display(); ?><br>
 		<input type="submit" value="<?php echo _AT('save'); ?>"   name="submit" accesskey="s" />
 		<input type="submit" value="<?php echo _AT('cancel'); ?>" name="cancel" />
 	</div>
