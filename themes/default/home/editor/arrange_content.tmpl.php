075b257a621d2238e004afb48f1db22836dd6960
Metamorfosec <33624021+metamorfosec@users.noreply.github.com>
2018-09-23 17:53:55 +0700

    Update csrf_class Path

--- themes/default/home/editor/arrange_content.tmpl.php
+++ themes/default/home/editor/arrange_content.tmpl.php
@@ -17,7 +17,7 @@ if (!defined('TR_INCLUDE_PATH')) { exit; }
 global $contentManager;
 
 require(TR_INCLUDE_PATH.'header.inc.php');
-require_once(TR_ClassCSRF_PATH.'class_csrf.php');
+require_once('../../class_csrf.php');
 ?>
 <div class="input-form">
 <form action="<?php echo $_SERVER['PHP_SELF'].'?_course_id='.$this->course_id; if ($this->cid > 0) echo SEP.'_cid='.$this->cid; else if ($this->pid > 0) echo SEP.'pid='.$this->pid;?>" method="post" name="form"> 
--- themes/default/home/editor/edit_content_struct.tmpl.php
+++ themes/default/home/editor/edit_content_struct.tmpl.php
@@ -18,7 +18,7 @@ if (!defined('TR_INCLUDE_PATH')) { exit; }
 global $onload;
 $onload = 'document.form.title.focus();';
 
-require_once(TR_ClassCSRF_PATH.'class_csrf.php');
+require_once('../../class_csrf.php');
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF'].'?'; if ($this->cid > 0) echo '_cid='.$this->cid; else if ($this->pid > 0) echo 'pid='.$this->pid.SEP.'_course_id='.$this->course_id; else echo '_course_id='.$this->course_id;?>" method="post" name="form"> 
 <div class="input-form" style="width:95%;margin-left:1.5em;">
